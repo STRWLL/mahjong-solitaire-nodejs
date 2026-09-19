3164f4dd8bc81c8aac8c374730891a8cc806ab5d
DESKTOP-5L0UP05\user <takeshi.sw.2001@gmail.com>
2026-09-19 13:17:39 +0900

    カンの制限の修正

--- game.js
+++ game.js
@@ -198,7 +198,7 @@ export class Game {
         if (
           choice instanceof Kan
           && (
-            HandEvaluator.cacheable(hand.kanPreview(choice.tile)).finishers
+            Tile.serialize(HandEvaluator.cacheable(hand.kanPreview(choice.tile)).finishers)
             !== Tile.serialize(hand.lastFinishers)
           )
         ) {
